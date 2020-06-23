7cf02cafc2d58e38eb67d684a4583cac2848b6e6
levisanchez <levijames@gmail.com>
2020-06-23 15:24:02 -0600

    Added tag resources

--- src/main/java/edu/cnm/deepdive/quotes/model/entity/Quote.java
+++ src/main/java/edu/cnm/deepdive/quotes/model/entity/Quote.java
@@ -1,6 +1,8 @@
 package edu.cnm.deepdive.quotes.model.entity;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,7 +11,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OrderBy;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import org.hibernate.annotations.CreationTimestamp;
@@ -20,25 +25,27 @@ import org.springframework.lang.NonNull;
 @Entity
 public class Quote {
 
+  @ManyToMany(fetch = FetchType.EAGER,
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @JoinTable(name = "quote_tag", joinColumns = @JoinColumn(name = "quote_id"),
+      inverseJoinColumns = @JoinColumn(name = "tag_id"))
+  @OrderBy("name ASC")
+  public List<Tag> tags = new LinkedList<>();
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "quote_id", nullable = false, updatable = false)
   private Long id;
-
   @CreationTimestamp
   @Temporal(TemporalType.TIMESTAMP)
   @Column(nullable = false, updatable = false)
   private Date created;
-
   @UpdateTimestamp
   @Temporal(TemporalType.TIMESTAMP)
   @Column(nullable = false)
   private Date updated;
-
   @NonNull
   @Column(length = 4096, nullable = false)
   private String text;
-
   @ManyToOne(fetch = FetchType.EAGER,
       cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
   @JoinColumn(name = "source_id")
@@ -73,4 +80,7 @@ public class Quote {
     this.source = source;
   }
 
+  public List<Tag> getTags() {
+    return tags;
+  }
 }
