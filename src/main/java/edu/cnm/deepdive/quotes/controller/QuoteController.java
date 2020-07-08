170caa6c76bb9797167066dc4aa7d33176efa660
levisanchez <levijames@gmail.com>
2020-07-08 16:57:27 -0600

    Added Exception Handler

--- src/main/java/edu/cnm/deepdive/quotes/controller/QuoteController.java
+++ src/main/java/edu/cnm/deepdive/quotes/controller/QuoteController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -66,4 +67,8 @@ public class QuoteController {
     return quoteRepository.findById(id).orElseThrow(NoSuchElementException::new);
   }
 
+  @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Iterable<Quote> search(@RequestParam(name = "q", required = true) String filter) {
+    return quoteRepository.getAllByTextContainsOrderByTextAsc(filter);
+  }
 }
