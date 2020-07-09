61d554c00007bb334c9ab0637eadda15793aa709
levisanchez <levijames@gmail.com>
2020-07-09 16:34:22 -0600

    Integrated HATEOS for resource URLs.

--- src/main/java/edu/cnm/deepdive/quotes/view/FlatQuote.java
+++ src/main/java/edu/cnm/deepdive/quotes/view/FlatQuote.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.quotes.view;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.net.URI;
 import java.util.Date;
 
 @JsonPropertyOrder({"id", "created", "updated", "text"})
@@ -13,5 +14,7 @@ public interface FlatQuote {
   Date getUpdated();
 
   String getText();
+
+  URI getHref();
 
 }
